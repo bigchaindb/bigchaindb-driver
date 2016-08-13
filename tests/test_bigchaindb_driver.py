903fbdbbeab0ec79cd9ad26e9739d930a1685875
Sylvain Bellemare <sylvain@ascribe.io>
2016-08-13 02:17:27 +0200

    Make some adjustments to the tests

--- tests/test_bigchaindb_driver.py
+++ tests/test_bigchaindb_driver.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """
 test_bigchaindb_driver
@@ -10,36 +9,8 @@ Tests for `bigchaindb_driver` module.
 
 import pytest
 
-from bigchaindb import config, util
+from bigchaindb import config_utils, util
 from bigchaindb.exceptions import KeypairNotFoundException
-
-
-@pytest.fixture
-def client():
-    from bigchaindb_driver.bigchaindb_driver import temp_client
-    return temp_client()
-
-
-@pytest.fixture
-def mock_requests_post(monkeypatch):
-    class MockResponse:
-        def __init__(self, json):
-            self._json = json
-
-        def json(self):
-            return self._json
-
-    def mockreturn(*args, **kwargs):
-        return MockResponse(kwargs.get('json'))
-
-    monkeypatch.setattr('requests.post', mockreturn)
-
-@pytest.fixture
-def mock_bigchaindb_sign(monkeypatch):
-    def mockreturn(transaction, private_key, bigchain):
-        return transaction
-
-    monkeypatch.setattr('bigchaindb.util.sign_tx', mockreturn)
 
 
 def test_temp_client_returns_a_temp_client():
@@ -49,25 +20,24 @@ def test_temp_client_returns_a_temp_client():
     assert client.private_key
 
 
-@pytest.mark.skip(reason='until fixture/config is worked out')
-@pytest.mark.usefixtures('restore_config')
-def test_client_can_create_assets(mock_requests_post, client):
-
+@pytest.mark.usefixtures('restore_config', 'mock_requests_post')
+def test_client_can_create_assets(client):
     tx = client.create()
 
-    # XXX: `CREATE` operations require the node that receives the transaction to modify the data in
-    #      the transaction itself.
-    #      `current_owner` will be overwritten with the public key of the node in the federation
-    #      that will create the real transaction. `signature` will be overwritten with the new signature.
-    #      Note that this scenario is ignored by this test.
+    # XXX: `CREATE` operations require the node that receives the transaction
+    #   to modify the data in the transaction itself.
+    #   `current_owner` will be overwritten with the public key of the node
+    #   in the federation that will create the real transaction. `signature`
+    #   will be overwritten with the new signature. Note that this scenario is
+    #   ignored by this test.
     assert tx['transaction']['fulfillments'][0]['current_owners'][0] == client.public_key
     assert tx['transaction']['conditions'][0]['new_owners'][0] == client.public_key
     assert tx['transaction']['fulfillments'][0]['input'] is None
-
     assert util.validate_fulfillments(tx)
 
 
-def test_client_can_transfer_assets(mock_requests_post, mock_bigchaindb_sign, client):
+@pytest.mark.usefixtures('mock_requests_post', 'mock_bigchaindb_sign')
+def test_client_can_transfer_assets(client):
     tx = client.transfer(client.public_key, 123)
     assert tx['transaction']['fulfillments'][0]['current_owners'][0] == client.public_key
     assert tx['transaction']['conditions'][0]['new_owners'][0] == client.public_key
@@ -78,6 +48,9 @@ def test_client_can_transfer_assets(mock_requests_post, mock_bigchaindb_sign, cl
     (None, None), ('pubkey', None), (None, 'privkey'),
 ))
 def test_init_client_with_incomplete_keypair(pubkey, privkey, monkeypatch):
+    # FIXME importing the config locally is needed in
+    # order to mock the latest config dict
+    from bigchaindb import config
     from bigchaindb_driver.bigchaindb_driver import Client
     keypair = {'public': pubkey, 'private': privkey}
     monkeypatch.setitem(config, 'keypair', keypair)
