0a74fb8eac8548664fbd8b5671cc6736f7c3bedb
Sylvain Bellemare <sbellem@gmail.com>
2016-08-19 11:25:32 +0200

    Cleanup server code dependencies removal (#16)
    * Remove left-over server code dependencies in tests
    
    * Remove unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ from os import environ
 
 from pytest import fixture
 
-from bigchaindb import config_utils
 from bigchaindb_common.transaction import Condition, Fulfillment, Transaction
 from cryptoconditions import Ed25519Fulfillment
 
@@ -38,12 +37,6 @@ def bob_keypair(bob_privkey, bob_pubkey):
 
 
 @fixture
-def keypair():
-    return ('CT6nWhSyE7dF2znpx3vwXuceSrmeMy9ChBfi9U92HMSP',
-            'G7J7bXF8cqSrjrxUKwcF8tCriEKC5CgyPHmtGwUi4BK3')
-
-
-@fixture
 def bdb_host():
     return environ.get('BDB_HOST', 'localhost')
 
@@ -51,21 +44,6 @@ def bdb_host():
 @fixture
 def bdb_node(bdb_host):
     return 'http://{}:9984/api/v1'.format(bdb_host)
-
-
-@fixture
-def node_config():
-    return {
-        'keypair': {
-            'private': '31Lb1ZGKTyHnmVK3LUMrAUrPNfd4sE2YyBt3UA4A25aA',
-            'public': '4XYfCbabAWVUCbjTmRTFEu2sc3dFEdkse4r6X498B1s8'
-        }
-    }
-
-
-@fixture
-def restore_config(node_config):
-    config_utils.set_config(node_config)
 
 
 @fixture
