e5ed8f230c1228f803a3a41262ae18e79e747c96
Sylvain Bellemare <sylvain@ascribe.io>
2016-08-23 16:48:41 +0200

    Document lib - #23

--- bigchaindb_driver/pool.py
+++ bigchaindb_driver/pool.py
@@ -2,10 +2,24 @@ class Pool:
     """Pool of connections."""
 
     def __init__(self, connections):
+        """Initializes a :class:`~bigchaindb_driver.pool.Pool` instance.
+
+        Args:
+            connections (list): List of
+                :class:`~bigchaindb_driver.connection.Connection`
+                instances.
+
+        """
         self.connections = connections
 
     def get_connection(self):
-        """Gets a connection from the pool."""
+        """Gets a connection from the pool.
+
+        Returns:
+            A :class:`~bigchaindb_driver.connection.Connection`
+            instance.
+
+        """
         if len(self.connections) > 1:
             raise NotImplementedError
 
--- bigchaindb_driver/transport.py
+++ bigchaindb_driver/transport.py
@@ -3,21 +3,44 @@ from .pool import Pool
 
 
 class Transport:
+    """Transport class."""
 
     def __init__(self, *nodes):
+        """Initializes an instance of
+        :class:`bigchaindb_driver.transport.Transport`.
+
+        Args:
+            nodes: nodes
+
+        """
         self.nodes = nodes
         self.init_pool(nodes)
 
     def init_pool(self, nodes):
-        """Initialize the pool of connections."""
+        """Initializes the pool of connections."""
         connections = [Connection(node_url=node) for node in nodes]
         self.pool = Pool(connections)
 
     def get_connection(self):
-        """Get a connection from the pool."""
+        """Gets a connection from the pool.
+
+        Returns:
+            A :class:`bigchaindb_driver.connection.Connection` instance.
+        """
         return self.pool.get_connection()
 
     def forward_request(self, method, path=None, json=None):
-        """Forwards an http request to a connection."""
+        """Forwards an http request to a connection.
+
+        Args:
+            method (str): HTTP method name (e.g.: ``'GET'``.
+            path (str): Path to be appended to the base url of a node. E.g.:
+                ``'/transactions'``.
+            json (dict): Payload to be sent with the HTTP request.
+
+        Returns:
+            dict: Result of :meth:`requests.models.Response.json`
+
+        """
         connection = self.get_connection()
         return connection.request(method=method, path=path, json=json)
