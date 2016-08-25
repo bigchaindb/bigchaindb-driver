16499f321ae49b29e32bb0c8875094ecd06d2696
Sylvain Bellemare <sylvain@ascribe.io>
2016-08-25 18:55:12 +0200

    Bump version: 0.0.2.dev2 → 0.0.2.dev3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ dependency_links = [
 
 setup(
     name='bigchaindb_driver',
-    version='0.0.2.dev2',
+    version='0.0.2.dev3',
     description="Python driver for BigchainDB",
     long_description=readme + '\n\n' + history,
     author="BigchainDB",
