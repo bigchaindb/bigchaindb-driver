a623f580782e60cc0a6a629219836e9ee15c75a2
Sylvain Bellemare <sylvain@ascribe.io>
2016-10-18 19:21:16 +0200

    Bump version: 0.0.2.dev10 → 0.0.2.dev11

--- bigchaindb_driver/__init__.py
+++ bigchaindb_driver/__init__.py
@@ -3,4 +3,4 @@ from .driver import BigchainDB   # noqa
 
 __author__ = 'BigchainDB'
 __email__ = 'dev@bigchaindb.com'
-__version__ = '0.0.2.dev10'
+__version__ = '0.0.2.dev11'
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ docs_require = [
 
 setup(
     name='bigchaindb_driver',
-    version='0.0.2.dev10',
+    version='0.0.2.dev11',
     description="Python driver for BigchainDB",
     long_description=readme + '\n\n' + history,
     author="BigchainDB",
