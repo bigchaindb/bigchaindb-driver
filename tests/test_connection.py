9c507eebc4530a2c87161450ff66b2311b2508cb
Sylvain Bellemare <sylvain@ascribe.io>
2017-01-25 15:37:54 +0100

    Test custom headers

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,8 +1,18 @@
 from pytest import mark
+from requests.utils import default_headers
 from responses import RequestsMock
 
 
 class TestConnection:
+
+    def test_init_with_custom_headers(self):
+        from bigchaindb_driver.connection import Connection
+        url = 'http://dummy'
+        custom_headers = {'app_id': 'id_value', 'app_key': 'key_value'}
+        connection = Connection(node_url=url, headers=custom_headers)
+        expected_headers = default_headers()
+        expected_headers.update(custom_headers)
+        assert connection.session.headers == expected_headers
 
     @mark.parametrize('content_type,json,data', (
         ('application/json', {'a': 1}, {'a': 1}),
@@ -18,3 +28,17 @@ class TestConnection:
         assert response.status_code == 200
         assert response.headers['Content-Type'] == content_type
         assert response.data == data
+
+    @mark.parametrize('headers', (
+        {}, {'app_name': 'name'}, {'app_id': 'id', 'app_key': 'key'}
+    ))
+    def test_request_with_headers(self, headers):
+        from bigchaindb_driver.connection import Connection
+        url = 'http://dummy'
+        connection = Connection(node_url=url, headers=headers)
+        with RequestsMock() as requests_mock:
+            requests_mock.add('GET', url, adding_headers=headers)
+            response = connection.request('GET')
+        assert response.status_code == 200
+        del response.headers['Content-type']
+        assert response.headers == headers
