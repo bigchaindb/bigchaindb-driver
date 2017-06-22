e173d87a7d0f22343925da57c4622c156253eb52
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-06-22 22:38:21 +0200

    updated test env

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -6,7 +6,6 @@ if [ "${TOXENV}" == "py35" ]; then
     rethinkdb --daemon
     export BIGCHAINDB_KEYPAIR_PUBLIC=GW1nrdZm4mbVC8ePeiGWz6DqHexqewqy5teURVHi3RG4
     export BIGCHAINDB_KEYPAIR_PRIVATE=2kQgBtQnHoauw8QchKM7xYvEBW1QDoHzhBsCL9Vi1AzB 
-    export BIGCHAINDB_DATABASE_BACKEND=rethinkdb
 
     # Start BigchainDB in the background and ignore any output
     bigchaindb start >/dev/null 2>&1 &
