a70fdb08c159cb751f8a64f8b6d77491797e51e0
Sylvain Bellemare <sylvain@ascribe.io>
2017-06-23 13:53:11 +0200

    Update bigchaindb (server) version to 1.0.0rc1

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -7,6 +7,6 @@ pip install --upgrade tox
 
 if [ "${TOXENV}" == "py35" ]; then
     sudo apt-get install rethinkdb
-    pip install git+https://github.com/bigchaindb/bigchaindb.git
+    pip install bigchaindb==1.0.0rc1
     pip install --upgrade codecov
 fi
