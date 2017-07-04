1e10869c2322de02666426de26aed2a6d3d0c7c0
Sylvain Bellemare <sylvain@ascribe.io>
2017-07-04 16:09:27 +0200

    Add mongodb

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -3,10 +3,10 @@
 set -e -x
 
 pip install --upgrade pip
-pip install --upgrade tox
 
-if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
-    sudo apt-get install rethinkdb
-    pip install bigchaindb
+if [[ -n ${TOXENV} ]]; then
+    pip install --upgrade tox
+else
+    pip install .[test]
     pip install --upgrade codecov
 fi
