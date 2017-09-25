7b5f5f6db3ca19c9e8aedf9c5265d6e2b1c4bedd
Sylvain Bellemare <sbellem@gmail.com>
2017-09-25 16:17:24 +0200

    fix

--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -7,6 +7,8 @@ if [[ "${BIGCHAINDB_DATABASE_BACKEND}" == rethinkdb ]]; then
     rethinkdb --daemon
     export BIGCHAINDB_KEYPAIR_PUBLIC=GW1nrdZm4mbVC8ePeiGWz6DqHexqewqy5teURVHi3RG4
     export BIGCHAINDB_KEYPAIR_PRIVATE=2kQgBtQnHoauw8QchKM7xYvEBW1QDoHzhBsCL9Vi1AzB
+    # Start BigchainDB in the background and ignore any output
+    bigchaindb start >/dev/null 2>&1 &
 elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
     # Connect to MongoDB on port 27017 via a normal, unsecure connection if
     # BIGCHAINDB_DATABASE_SSL is unset.
@@ -16,7 +18,6 @@ elif [[ "${BIGCHAINDB_DATABASE_BACKEND}" == mongodb ]]; then
     mkdir /tmp/mongodb-data
     ${PWD}/mongodb-linux-x86_64-ubuntu1404-3.4.4/bin/mongod \
         --dbpath=/tmp/mongodb-data --replSet=bigchain-rs &> /dev/null &
+    # Start BigchainDB in the background and ignore any output
+    bigchaindb start >/dev/null 2>&1 &
 fi
-
-# Start BigchainDB in the background and ignore any output
-bigchaindb start >/dev/null 2>&1 &
