551735c5427586dbc0608c1e76101c5cf2eeac35
Sylvain Bellemare <sbellem@gmail.com>
2017-09-25 16:53:05 +0200

    fix

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
+#if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
+if [[ "${TOXENV}" == *-rdb ]]; then
     source /etc/lsb-release
     echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee -a /etc/apt/sources.list.d/rethinkdb.list
     wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
