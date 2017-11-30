eb9937497ef0d10c2521758e9743d094c0a296c3
Sylvain Bellemare <sbellem@gmail.com>
2017-11-30 01:42:19 +0100

    Fulfill and hash tx changes

--- tests/test_offchain.py
+++ tests/test_offchain.py
@@ -73,19 +73,19 @@ def test_prepare_create_transaction(asset, signers, recipients):
     ('2dBVUoATxEzEqRdsi64AFsJnn2ywLCwnbNwW7K9BuVuS',),
     [(['2dBVUoATxEzEqRdsi64AFsJnn2ywLCwnbNwW7K9BuVuS'], 1)],
 ))
-def test_prepare_transfer_transaction(alice_transaction, recipients):
+def test_prepare_transfer_transaction(signed_alice_transaction, recipients):
     from bigchaindb_driver.offchain import prepare_transfer_transaction
     condition_index = 0
-    condition = alice_transaction['outputs'][condition_index]
+    condition = signed_alice_transaction['outputs'][condition_index]
     input_ = {
         'fulfillment': condition['condition']['details'],
         'fulfills': {
             'output_index': condition_index,
-            'transaction_id': alice_transaction['id'],
+            'transaction_id': signed_alice_transaction['id'],
         },
         'owners_before': condition['public_keys']
     }
-    asset = {'id': alice_transaction['id']}
+    asset = {'id': signed_alice_transaction['id']}
     transfer_transaction = prepare_transfer_transaction(
         inputs=input_, recipients=recipients, asset=asset)
     assert 'id' in transfer_transaction
