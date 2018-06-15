d835b1dd7e2a55c84d2abfa658edf933ee42565e
codegeschrei <codegeschrei@gmail.com>
2018-06-15 14:15:37 +0200

    test xml

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
-  docker-compose run --rm bigchaindb-driver pytest -v --cov=bigchaindb_driver --cov-report html
+  docker-compose run --rm bigchaindb-driver pytest -v --cov=bigchaindb_driver --cov-report xml
   bash <(curl -s https://codecov.io/bash)
 else
   tox -e ${TOXENV}
