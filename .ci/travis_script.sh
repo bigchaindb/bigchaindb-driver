6dc229ec210655f7521ff6535a0f19d27cff9c10
codegeschrei <codegeschrei@gmail.com>
2018-06-18 14:44:01 +0200

    change location of xml

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
-  docker-compose run --rm bigchaindb-driver pytest -v --cov=bigchaindb_driver --cov-report xml
+  docker-compose run --rm bigchaindb-driver pytest -v --cov=bigchaindb_driver --cov-report xml:htmlcov/coverage.xml
   bash <(curl -s https://codecov.io/bash)
 else
   tox -e ${TOXENV}
