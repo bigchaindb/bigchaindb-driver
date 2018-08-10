2ae4885e96beb072e52787f2373074a0384fd316
Troy McConaghy <troy@bigchaindb.com>
2018-08-10 14:42:42 +0200

    Problem: No comments re/ licensing in source files (#465)
    Solution: Add license info comments to all source files

--- .ci/travis-before-install.sh
+++ .ci/travis-before-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright BigchainDB GmbH and BigchainDB contributors
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
 
 if [[ "${TOXENV}" == "py35" || "${TOXENV}" == "py36" ]]; then
   sudo apt-get update
--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright BigchainDB GmbH and BigchainDB contributors
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
 
 from setuptools import setup, find_packages
 
