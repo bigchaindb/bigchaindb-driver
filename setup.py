b15d093d63102408579350ec7500c5edfdf2a423
Troy McConaghy <troy@bigchaindb.com>
2018-11-03 16:10:58 +0100

    Update minimum version of requests package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('CHANGELOG.rst') as changelog_file:
     changelog = changelog_file.read()
 
 install_requires = [
-    'requests>=2.11.0',
+    'requests>=2.20.0',
     'cryptoconditions==0.8.0',
     'pysha3~=1.0.2',
     'python-rapidjson~=0.6.0',
