0b8bfad1a19a93b255be2d92ef6d29c047120389
David Dashyan <mail@davie.li>
2020-12-08 19:34:04 +0300

    Add docstrings
    Signed-off-by: David Dashyan <mail@davie.li>

--- bigchaindb_driver/common/transaction.py
+++ bigchaindb_driver/common/transaction.py
@@ -837,12 +837,25 @@ class Transaction(object):
         return self
 
     def delegate_signing(self, callback):
+        """Fulfills a previous Transaction's Output by signing Inputs using callback.
+
+            Note:
+                This method works only for the following Cryptoconditions
+                currently:
+                    - Ed25519Fulfillment
+            Args:
+                callback (function): A callback used to sign inputs. Callback
+                    takes tx dict and message to sign as arguments and returns
+                    signature (bytes).
+            Returns:
+                :class:`~bigchaindb.common.transaction.Transaction`
+        """
         tx_dict = self.to_dict()
         tx_dict = self._remove_signatures(tx_dict)
         tx_serialized = self._to_str(tx_dict)
         message = sha3_256(tx_serialized.encode())
         for input_ in self.inputs:
-            if input_.fulfills:  # ??? What is this bit for?
+            if input_.fulfills:
                 message.update('{}{}'.format(
                     input_.fulfills.txid, input_.fulfills.output).encode())
             signature = callback(tx_dict, message.digest())
